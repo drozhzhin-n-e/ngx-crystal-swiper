4de3d79801b91a88d39e75223613fe573ec9ffca
drozhzhin-n-e <drozhzhin.n.e@mail.ru>
2018-04-15 09:22:51 +0300

    Correcting file addresses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,40 +9,40 @@ export class AppComponent {
 
 	myImages: any = [
 		{
-			path: '/assets/img/yi-sung-tsai-04--vertical.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-04--vertical.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-01.jpg',
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-01.jpg',
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-02.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-02.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-03.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-03.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-04.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-04.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-05.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-05.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-06.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-06.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-07.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-07.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-08.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-08.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-09.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-09.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-10.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-10.jpg'
 		},
 		{
-			path: '/assets/img/yi-sung-tsai-11.jpg'
+			path: 'https://drozhzhin-n-e.github.io/ngx-crystal-swiper/assets/img/yi-sung-tsai-11.jpg'
 		}
 	];
 }
